7baa9d3dacee4e307cfd8ab29ae2db4141664049
Urs Mazenauer <umazenauer@hotmail.com>
2019-12-09 21:05:42 +0100

    initial styling

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -1,14 +1,16 @@
 import {h, Component} from '@stencil/core';
 
 @Component({
-    tag: 'um-side-drawer'
+    tag: 'um-side-drawer',
+    styleUrl: './side-drawer.css',
+    shadow: true
 })
 export class SideDrawer {
     render() {
         return (
-            <div>
+            <aside>
                 <h1>The Side Drawer</h1>
-            </div>
+            </aside>
         );
     }
 }
